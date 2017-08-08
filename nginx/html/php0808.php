737797c4e60833b13c33c3e76112d4827fbfffdf
syn <1416533671@qq.com>
2017-08-08 18:34:04 +0800

    添加js文件和php文件

---
+++ nginx/html/php0808.php
@@ -0,0 +1,18 @@
+<!--
+<?php
+phpinfo();
+?>
+-->
+
+<html>
+    <head>
+        <title>JS开始</title>
+        <script type="text/javascript" src="js/js0808.js"></script> 
+            <!--        <script type="text/javascript" src="js/jquery.min.js"></script>-->
+    </head>
+    
+    <body>
+
+    </body>
+    
+</html>
