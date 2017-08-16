f2cffac17c352c1daa3bde46bdd0f35b2046fb9d
syn <1416533671@qq.com>
2017-08-16 10:55:27 +0800

    0815学习（今天局jb忙）

---
+++ nginx/html/php0815.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Example</title>
+        <link type="text/css" rel="stylesheet" href="css/CssTongYong.css">
+    </head>
+    <body>
+
+        <?php
+        echo "Hi, I'm a PHP scsript!";
+        ?>
+
+        <script>document.write("<p>" + "date" + "</p>") </script>
+        
+        <?php
+            
+        ?>
+        
+        
+    </body>
+</html>
