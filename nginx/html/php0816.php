54b9a846e54b09ab1002c86376b79450f3ce56a2
syn <1416533671@qq.com>
2017-08-17 17:46:22 +0800

    0816学习，不过还没咋学

---
+++ nginx/html/php0816.php
@@ -0,0 +1,28 @@
+<tr>
+              <td height="45"  align="left" class="textstyle3"><label>输出</label></td>
+            </tr>
+            <tr>
+              <td height="35" width="30%" align="center" style="font-size:16px;color:#000000">                  <input type="radio" name="output"  id="outputASI" class="radio" checked="checked"/>
+                  <label> TS over ASI</label>              </td>
+			  <td align="left" colspan="3"><select name="selectoutputset"  id="selectoutputset" style="WIDTH: 230px">
+              </select></td>
+            </tr>
+            <tr>
+				<td height="35" width="30%" align="center" style="font-size:16px;color:#000000">
+				  <input type="radio" name="output"  id="outputIP" class="radio"/>
+               	<label> TS over IP</label>              </td>
+                <td align="left" colspan="3"><input name="text4" type="text" id="textfieldoutputnameset" style="WIDTH: 230px;HEIGHT:23px" value="udp://192.168.3.169:9002" /></td>
+            </tr>
+            <tr>
+              <td height="45"  align="left" class="textstyle3"><label>状态简析</label></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td height="35" align="left" colspan="3" class="textstyle5" id="textfieldstatus">encoder</td>
+            </tr>
+
+ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
+
+    
+    
+    
